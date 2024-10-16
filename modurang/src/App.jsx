90b55347f78059e3df6b4b0d24205c32067ecc4e
joungminsung <joungminsung080830@gmail.com>
2024-10-16 20:05:23 +0900

    Update App.jsx

--- modurang/src/App.jsx
+++ modurang/src/App.jsx
@@ -1,8 +1,10 @@
 import React, { useEffect } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Login from './pages/Login/index';
-import Signup from './pages/SignUpPage/index'
-import Notice from './pages/Notice/index'
+import Signup from './pages/SignUpPage/index';
+import Notice from './pages/Notice/index';
+import Main from './pages/MainPage/index'
+
   
 function App() {
   useEffect(() => {
@@ -15,6 +17,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
         <Route path='/notice' element={<Notice/>}/>
+        <Route path="/main" element={<Main/>} />
       </Routes>
     </BrowserRouter>
   );
